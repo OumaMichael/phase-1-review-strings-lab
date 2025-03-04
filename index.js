326bb12609e7de66fac3bf114a6c412ef7ca0a68
Michael onyango ouma <oumamichael108@gmail.com>
2025-03-04 18:17:02 +0300

    Complete strings lab: define excitedWelcomeMessage and shortGreeting

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Write your code in this file!
+const currentUser = 'Grace Hopper';
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+//welcomeMessage becomes: "Welcome to Flatbook, Grace Hopper!"
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+//excitedWelcomeMessage becomes: "WELCOME TO FLATBOOK, GRACE HOPPER!"
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`
+//The .slice(0, 1) method extracts a portion of the string, starting at index 0 and stopping before index 1. This effectively gets just the first character of currentUser (which is "G" in "Grace Hopper").
